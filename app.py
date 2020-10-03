d9562b84d2431753c597d9f38a32a58c7821ad6d
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-10-03 17:31:15 -0600

    Flask template website running without issues

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html') 
+
+@app.route("/tool", methods = ['GET'])
+def tool():
+	return render_template('tool.html')

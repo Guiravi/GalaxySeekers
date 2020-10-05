e30f02b108c116fd4d61a8b5561876e8084b5275
Marco Mora <marco8mora@gmail.com>
2020-10-04 19:22:11 -0600

    Slider functionality added

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ def index():
 
 @app.route("/tool", methods = ['GET'])
 def tool():
-    #radio_images = readLinkFile('radio.txt')
+    radio_images = readLinkFile('radio.txt')
     microwave_images = readLinkFile('millimeter.txt')
-    #optical_images = readLinkFile('optical.txt')
-    #infrared_images = readLinkFile('infrared.txt')
-    #uv_images = readLinkFile('uv.txt')
-    #infrared_images = readLinkFile('radio.txt')
+    optical_images = readLinkFile('optical.txt')
+    infrared_images = readLinkFile('infrared.txt')
+    ultrav_images = readLinkFile('uv.txt')
+    gammar_images = readLinkFile('gamma-ray.txt')
+    xray_images = readLinkFile('x-ray.txt')
 
-    return render_template('tool.html', micro_imgs = microwave_images)
+    return render_template('tool.html', micro_imgs = microwave_images, rad_imgs = radio_images, opt_images = optical_images, infr_images = infrared_images, uv_images = ultrav_images, gamma_images = gammar_images, xr_images = xray_images)
 
 def readLinkFile(filename):
 	file = open(filename, 'r')
